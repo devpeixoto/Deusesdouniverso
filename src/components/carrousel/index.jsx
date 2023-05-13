29d6b3b0a5311660a2ec6c4a1401c2cec1cf8945
Gabriel <Gabriel@dllinformatica.local>
2023-05-12 23:26:17 -0300

    adicionado comentario

--- src/components/carrousel/index.jsx
+++ src/components/carrousel/index.jsx
@@ -4,7 +4,7 @@ import "./style.css"
 import { motion } from "framer-motion";
 
 
-import imge1 from '../../../public/img/capa1.jpg'
+import imge1 from '../../../public/img/tininha.jpeg'
 import imge2 from '../../../public/img/logo.jpg'
 import imge3 from '../../../public/img/1682461705027.jpg'
 
@@ -12,7 +12,8 @@ import imge3 from '../../../public/img/1682461705027.jpg'
 const images = [imge1, imge2, imge3]
 const history = [
     'Lorem ipsum dolor sit amet . Os operadores gráficos e tipográficos sabem disso bem, na realidade, todas as profissões que lidam com o universo da comunicação têm um relacionamento estável com essas palavras, mas o que é? Lorem ipsum é um texto fofo sem qualquer sentido.',
-    
+    'Texto 2 não sei o que vai ser desse texto mas irei colocar da melhor forma possível pois isso tudo só depende de mim para dar certo',
+    'texto 3 vamos la, vou mostrar que sou capaz de tudo nessa vida, já sou um vencedor por lutar por tudo aquilo que eu quiz! e não preciso mostrar pra ninguém.'
 ]
 
 function CarSlid( ) {
@@ -44,7 +45,22 @@ function CarSlid( ) {
                             <img src={image} alt="Texto" />
                         </motion.div>
                     ))}
+            
 
+                </motion.div>
+                <motion.div
+                 className="inner"
+                 drag="x"
+                 dragConstraints={{right:0, left: -width}}
+                 initial={{x:100}}
+                 animate={{x:0}}
+                 transition={{duration: 0.8}}
+                >
+                    {history.map(comments => (
+                        <motion.div className="item" key={comments}>
+                            <div children={comments} />
+                        </motion.div>
+                    ))}
                 </motion.div>
             </motion.div>
         </div>
